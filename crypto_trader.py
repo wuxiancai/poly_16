9f7c14e413d81a0bbd07647ddae0fa06d02b2e3c
poly-new <wuxiancai1978@gmail.com>
2025-08-21 00:06:53 +0800

    修复

--- crypto_trader.py
+++ crypto_trader.py
@@ -38,7 +38,7 @@ import psutil
 import socket
 import urllib.request
 import requests
-from trade_stats import TradeStatsManager
+from trade_stats_manager import TradeStatsManager
 import urllib3
 import warnings
 from collections import defaultdict
